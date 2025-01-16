53eae06814987e12f678f75e4a13182c96fd0f3c
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-16 14:51:07 +0530

    Add files via upload

---
+++ Constructor_example.java
@@ -0,0 +1,39 @@
+package corejava;
+
+
+
+class A
+{
+	int a; String b; boolean c;
+	A()                  //default constructor
+	{
+		a=50; b="ajay"; c=true;
+	}
+	void disp()
+	{
+		System.out.println(a+" "+b+" "+c);
+	}
+	
+	void show()
+	{
+		int a=11;
+		String b="Ajju";
+		String c="sam";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
+public class Constructor_example 
+{
+
+	public static void main(String[] args)
+	{
+		A c = new A();
+		c.disp();
+		c.show();
+
+	}
+
+}
