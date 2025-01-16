3afbb95ac1fd4cbe7f4f366787f1a5d6eac88b26
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-16 14:52:26 +0530

    Add files via upload

---
+++ Constructor_private_example.java
@@ -0,0 +1,27 @@
+package corejava;
+
+class Constructor_private_example
+{
+	int a; double b; String c;
+	private Constructor_private_example()             //Private Constructor
+	{
+		a=10; b=20.3; c="Pass";
+		System.out.println(a+" "+b+" "+c);
+	}
+	
+	static void show()
+	{
+		int a,b,c;
+		a=10; b=20;
+		c=a+b;
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args)
+	{
+		Constructor_private_example r=new Constructor_private_example();
+		r.show();
+
+	}
+
+}
