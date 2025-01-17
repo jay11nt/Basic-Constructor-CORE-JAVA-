5a1b4613a7498ac6f082132bc674339a407d4062
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-17 20:16:22 +0530

    Update Constructor_private_example.java

--- Constructor_private_example.java
+++ Constructor_private_example.java
@@ -13,12 +13,12 @@ class Constructor_private_example
 	    }
 	
 	static void show()
-	{
+	    {
 		int a,b,c;
 		a=10; b=20;
 		c=a+b;
 		System.out.println(c);
-	}
+	    }
 	
 	public static void main(String[] args)
 	{
