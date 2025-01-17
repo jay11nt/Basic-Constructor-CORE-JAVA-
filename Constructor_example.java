58b88b536a021cfcb6636c6f61cf0a7e3a7c87c6
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-17 20:18:13 +0530

    Update Constructor_example.java

--- Constructor_example.java
+++ Constructor_example.java
@@ -6,35 +6,35 @@ class A
 	String b;
 	boolean c;
 	
-	A()                  //default constructor
+	     A()                  //default constructor
 		{
-		a=50; b="ajay"; c=true;
+		   a=50; b="ajay"; c=true;
 		}
-	void disp()
+	
+	  void disp()
 		{
-		System.out.println(a+" "+b+" "+c);
+		  System.out.println(a+" "+b+" "+c);
 		}
 	
 	void show()
-		{
+	    {
 		int a=11;
 		String b="Ajju";
 		String c="sam";
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
-		}
+	    }
 }
 
 public class Constructor_example 
 {
 
 	public static void main(String[] args)
-	{
+	   {
 		A c = new A();
 		c.disp();
 		c.show();
-
-	}
+	   }
 
 }
